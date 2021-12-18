87094c376e6f40994365c6336449734902021894
mignez <93382579+profamignez@users.noreply.github.com>
2021-12-17 22:22:24 -0300

    Atividade "String" Avançada

---
+++ 4_string/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof 'Meu nome é Inez!');
+console.log(typeof "Eu queria viajar para a Europa");
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ 5_string _avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \n Segunda linha");
+console.log('Meu nome é "Inez"');
+console.log('O nome dela é "Roberta"');
+console.log(`A multiplicação de 5 por 3 é ${5*3} `);
+console.log("O " + "meu" + " nome " + "é" + " Matheus.");
