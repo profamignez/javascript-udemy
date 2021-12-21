3839940e8d6b6092425e35383ed1d225997c4570
mignez <93382579+profamignez@users.noreply.github.com>
2021-12-21 07:40:43 -0300

    Exercícios - Boolean (operadores lógicos)

---
+++ 12b_exercicio_02/12a_exercicio_01/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof("Maria" == "Ana"));
+//Três Tipos de String:
+console.log(typeof("Maria"));
+console.log('dia 25 é natal');
+console.log(`Este aqui é o Template Literals`)
---
+++ 12c_exercicio_03/scripts.js
@@ -0,0 +1,5 @@
+console.log(9 < 6) && (10 > 4);
+console.log(24 <= 14) || (12 > 18);
+console.log(!(7 > 16)) && (72 > 45); 
+console.log('Inez' != 'João');
+
