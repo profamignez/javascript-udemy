85b3f2690181f13af55f936b515701564c6bb558
mignez <93382579+profamignez@users.noreply.github.com>
2021-12-27 14:55:31 -0300

    Variáveis - let

---
+++ 13_variaveis/script.js
@@ -0,0 +1,21 @@
+let nome = "Inez";
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "João";
+console.log(nome);
+
+laranja = 845;
+console.log(laranja);
+
+laranja = "Laranja";
+console.log(laranja);
+
+let um = 1, dois = 2, tres = 3;// não é viáveis, pois o código fica difícil de ler
+console.log(um + dois + tres);
+
+
