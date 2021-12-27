be9176a90f98194ddcd7cd7eab679939692a4ac0
mignez <93382579+profamignez@users.noreply.github.com>
2021-12-27 15:16:22 -0300

    Variáveis - Convenção

---
+++ 15_variaveis_convencao/script.js
@@ -0,0 +1,8 @@
+let nome4 = 'teste';
+let $nome4 = 'teste2';
+let _nome4 = 'teste3';
+// let @nome =  'teste4';
+// let !nome =  'teste5';
+// let 4nome = "teste";
+let Nome = 'teste';
+let seuNome = 'teste';
